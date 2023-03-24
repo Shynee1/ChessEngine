3547b4472425b751aaf191f89f9a1860adfb14e1
Shynee1 <jackbarkerford@gmail.com>
2023-03-24 17:12:31 -0400

    Documented Evaluation class

--- core/src/com/shynee/main/chess/AI/Evaluation.java
+++ core/src/com/shynee/main/chess/AI/Evaluation.java
@@ -7,6 +7,10 @@ import com.shynee.main.chess.Square;
 
 import java.util.List;
 
+/**
+ * Evaluation -- Used to perform static evaluation of a given board position.
+ * Factors in material value, piece position, and endgames.
+ */
 public class Evaluation {
 
     private final static int pawnValue = 100;
@@ -15,7 +19,16 @@ public class Evaluation {
     private final static int rookValue = 500;
     private final static int queenValue = 900;
 
+    // Marks the start of the endgame
     private final static int endgameStart = rookValue*2+bishopValue+knightValue;
+
+    /**
+     * Performs an evaluation of the current board position.
+     * A positive value is good for whoever turn it is to move.
+     *
+     * @param board ChessBoard class representing current board.
+     * @return The evaluation.
+     */
     public static int evaluate(ChessBoard board){
         int whiteEval = countMaterial(board, true);
         int blackEval = countMaterial(board, false);
@@ -34,17 +47,35 @@ public class Evaluation {
         whiteEval += evalPieceSquareTables(board, true, blackEndWeight);
         blackEval += evalPieceSquareTables(board, false, whiteEndWeight);
 
-        //Negative = bad, positive = good
+        // Negative = bad, positive = good
         int perspective = board.colorToMove()?1:-1;
 
         return (whiteEval-blackEval)*perspective;
     }
 
+    /**
+     * A value that gets larger as players move farther into the endgame
+     * @param materialWithoutPawns Opponent's static material value without pawns.
+     * @return The evaluated endgame weight.
+     */
     private static float getEndgameWeight(int materialWithoutPawns){
         float multiplier = 1f/endgameStart;
         return 1 - Math.min(1, materialWithoutPawns*multiplier);
     }
 
+    /**
+     * Evaluates the position of the king in the endgame.
+     * Rewards the friendly king moving closer to the enemy king.
+     * Meant to assist with checkmating when search can't see
+     * far enough ahead for checkmate.
+     *
+     * @param friendlyKing Square of the friendly king.
+     * @param friendlyEval Current evaluation of the friendly color.
+     * @param opponentKing Square of the opponent king.
+     * @param opponentEval Current evaluation of the opponent color.
+     * @param endgameWeight Value that gets larger as opponent loses more pieces.
+     * @return Integer evaluation of the friendly king position.
+     */
     private static int kingEndgameEval(Square friendlyKing, int friendlyEval, Square opponentKing, int opponentEval, float endgameWeight){
         // Check if entered endgame
         if (!(friendlyEval > opponentEval + pawnValue * 2 && endgameWeight > 0)) return 0;
@@ -71,6 +102,12 @@ public class Evaluation {
         return (int) (endgameEval*endgameWeight);
     }
 
+    /**
+     * Sums the total material of all pieces of a color on the board.
+     * @param board ChessBoard class representing current board.
+     * @param color Color of the pieces to count.
+     * @return Sum of all material values.
+     */
     private static int countMaterial(ChessBoard board, boolean color){
         int material = 0;
 
@@ -83,6 +120,18 @@ public class Evaluation {
         return material;
     }
 
+    /**
+     * Evaluates the position of every piece of a certain color.
+     * Positions are evaluated using an array of values that represent every square of the board.
+     * These arrays are generalized rules about where pieces should be
+     * (e.g. king should stay on the bottom of the board and not wander).
+     * See PieceSquareTables class for all values.
+     *
+     * @param board ChessBoard class representing the current board position.
+     * @param color Color of the pieces to evaluate.
+     * @param endgameWeight Value that increases as opponent loses more pieces.
+     * @return Sum of the position evaluation of every piece of the specified color.
+     */
     private static int evalPieceSquareTables(ChessBoard board, boolean color, float endgameWeight){
         int value = 0;
 
@@ -99,6 +148,16 @@ public class Evaluation {
         return value;
     }
 
+    /**
+     * Evaluates position of all pieces of a certain type/color using piece-square tables.
+     * See above for more details.
+     *
+     * @param board ChessBoard class representing the current board position.
+     * @param table Piece-square table to evaluate.
+     * @param pieceType Type of piece to evaluate against table.
+     * @param color Color of the piece.
+     * @return Sum of the position evaluation of all pieces of the specified type and color.
+     */
     private static int evalPieceSquareTable(ChessBoard board, int[] table, int pieceType, boolean color){
         int value = 0;
         List<Integer> pieceList = board.getPiecePosList(pieceType, color);
